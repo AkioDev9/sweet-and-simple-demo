1effb4f0b737d491f5be2e7ca90353ec2cfeabaa
pieromendozadev <pieromendoza.dev@gmail.com>
2024-12-06 16:04:46 -0500

    navbar arreglada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import { LandingComponent } from "./components/landing/landing.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, MatMenuModule],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    LandingComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
