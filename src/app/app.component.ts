70edfbd9faf6f76a88a5b85486d96dbe7e11f7ed
AkioDev9 <akio.oshiro.dev@gmail.com>
2024-12-08 13:52:12 -0500

    About us v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { LandingComponent } from "./components/landing/landing.component";
+import { AboutComponent } from "./components/about/about.component";
 
 
 @Component({
@@ -13,8 +14,10 @@ import { LandingComponent } from "./components/landing/landing.component";
     MatButtonModule,
     MatMenuModule,
     MatIconModule,
-    LandingComponent
-],
+    LandingComponent,
+    RouterModule,
+    AboutComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
